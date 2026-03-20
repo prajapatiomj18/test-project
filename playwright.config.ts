2f467bd4bf2740ff3b1ad75e0d265f78394264de
Om Prajapati <omjprajapati18@gmail.com>
2026-03-20 11:27:37 +0530

    Your descriptive commit message

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['junit', { outputFile: 'test-results/results.xml' }],
+  ],
   use: {
     trace: 'on-first-retry',
   },
